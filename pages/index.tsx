3e4c8fa9539b5093f71aafd122af74bf534ec93a
tinuh <tinuvanapamula@gmail.com>
2022-11-11 15:20:09 -0500

    info and alignment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,38 @@
 import Head from "next/head";
 import Image from "next/image";
 import Calendar from "../components/Calendar";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { IoLocationSharp } from "react-icons/io5";
 
 export default function Home() {
 	return (
-		<div>
-			<h1 className="text-4xl text-center font-bold pt-5">
+		<div className="px-5 md:px-10">
+			<h1 className="text-xl md:text-4xl text-center font-bold pt-5">
 				Montgomery Blair High School
 			</h1>
-			<div className="flex flex-wrap justify-between p-5 md:px-20 gap-5">
-				<div>
-					<img
-						src="https://mbhs.edu/carousel/img3.png"
-						alt="MBHS"
-						className="rounded-lg w-full md:h-72"
-					/>
+			<div className="bg-neutral-200 rounded-lg p-2 my-5 w-max m-auto flex flex-wrap gap-5">
+				<p className="flex gap-2 items-center">
+					<IoLocationSharp />
+					51 University Blvd East
+				</p>
+				<p className="flex gap-2 items-center">
+					<BsFillTelephoneFill />
+					(240) 740-7200
+				</p>
+			</div>
+			{/* <div className="bg-neutral-200 rounded-lg p-2 my-5 w-max m-auto flex flex-wrap gap-5">
+				<p>Principal Renay Johnson</p>
+				<p>Home of the Blazers</p>
+			</div> */}
+			<div className="flex flex-wrap justify-between gap-5">
+				<div className="flex-1">
+					<div className="w-full">
+						<img
+							src="https://mbhs.edu/carousel/img3.png"
+							alt="MBHS"
+							className="rounded-lg w-full md:w-auto md:h-72 mx-auto"
+						/>
+					</div>
 				</div>
 				<Calendar />
 			</div>
