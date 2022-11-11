220d2dfdbec4adae72875ed9e86cc3ab2ff22a19
tinuh <tinuvanapamula@gmail.com>
2022-11-11 12:27:06 -0500

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div>
-			<h1>MBHS</h1>
+			<h1 className="text-4xl text-center font-bold pt-5">MBHS</h1>
 		</div>
 	);
 }
