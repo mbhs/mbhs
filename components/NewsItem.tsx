f3dcbc833305bb707aba1e6eaebb19c42be35c3d
Ace <yunac477@gmail.com>
2022-11-15 17:10:16 -0500

    fixed newsitem build

--- components/NewsItem.tsx
+++ components/NewsItem.tsx
@@ -1,4 +1,4 @@
-function NewsItem({ header, content }) {
+function NewsItem({ header, content }: { header: string, content: string }) {
     return (
     <div className="bg-gray-200 w-1/2 place-content-center m-12 content-center rounded-xl hover:bg-gray-300 transition-all  odd: float-left even:float-right">
         <div className="px-5 py-5">
