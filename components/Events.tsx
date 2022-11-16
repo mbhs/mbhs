298254840036af92c7f8f7292a5a7d0063c7565a
darkninja1 <dahonzak@gmail.com>
2022-11-16 17:56:06 -0500

    some fun edits to the events

--- components/Events.tsx
+++ components/Events.tsx
@@ -23,6 +23,27 @@ const colorSwitch = (interation: any) => {
 		return "bg-white";
 	}
 }
+const seasonSwitch = (thedate: any) => {
+	var year = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+	var nthedate = thedate.split(" ");
+	var nnthedate = year.indexOf(nthedate[0]);
+	if (nnthedate == 2 || nnthedate == 3 || nnthedate == 4) {
+		return "bg-[url('https://images.pexels.com/photos/72473/pexels-photo-72473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover";
+	}
+	else if (nnthedate == 5 || nnthedate == 6 || nnthedate == 7) {
+		return "bg-[url('https://images.pexels.com/photos/219998/pexels-photo-219998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover";
+	}
+	else if(nnthedate == 8 || nnthedate == 9 || nnthedate == 10){
+		return "bg-[url('https://images.freeimages.com/images/large-previews/470/fall-in-iran-1640635.jpg')] bg-cover";
+	}
+	else if (nnthedate == 11 || nnthedate == 0 || nnthedate == 1) {
+		return "bg-[url('https://images.pexels.com/photos/813871/pexels-photo-813871.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover";
+	}
+	else {
+		console.log(nthedate[0]);
+		return "bg-black";
+	}
+}
 
 export default function Events({ events }: EventsProps) {
 	React.useEffect(() => {
@@ -32,14 +53,14 @@ export default function Events({ events }: EventsProps) {
 	return (
 		<div className=" bg-gradient-to-r from-black to-red-500 pt-10 px-20 w-full md:w-100% min-h-screen lg:px-10">
 			<h1 className="text-4xl font-bold text-center pb-10 text-white drop-shadow-lg">Upcoming Events</h1>
-			<div className="flex flex-col flex-wrap gap-6">
+			<div className="flex flex-col flex-wrap gap-6 pb-10">
 				{events.map(({ attributes: { title, description, startDate } }, i) => (
 					<div
 						className={`rounded-lg p-5 ${colorSwitch(i)} text-sm drop-shadow-md`}
 						key={i}
 					>
 						<div className="flex items-center gap-3 mb-2">
-							<div className="bg-black rounded-lg p-2 flex-none w-16 h-16">
+							<div className={`${seasonSwitch(parseDate(startDate))} rounded-lg p-2 flex-none w-16 h-16`}>
 								<p className="text-center font-semibold text-white text-base">
 									{parseDate(startDate)}
 								</p>
