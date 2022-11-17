f0ea87a745080e4b845a5343bee87ad066daf9e8
darkninja1 <dahonzak@gmail.com>
2022-11-16 20:21:18 -0500

    added nav bar to events that does not work

--- components/Events.tsx
+++ components/Events.tsx
@@ -44,19 +44,23 @@ const seasonSwitch = (thedate: any) => {
 		return "bg-black";
 	}
 }
+const handleChange = (event: any) => {
+	console.log("hi");
+}
 
 export default function Events({ events }: EventsProps) {
 	React.useEffect(() => {
 		console.log(events);
-	}, []);
+	}, []);	
 //"alternate colors and put gradient" suggestion from student
 	return (
 		<div className=" bg-gradient-to-r from-black to-red-500 pt-10 px-20 w-full md:w-100% min-h-screen lg:px-10">
 			<h1 className="text-4xl font-bold text-center pb-10 text-white drop-shadow-lg">Upcoming Events</h1>
+			<input autoFocus onInput={handleChange} className="focus:ring focus:ring-red-500 flex bg-white rounded-full w-full relative mb-10 p-3 text-2xl outline-none" placeholder="Search Events..."></input>
 			<div className="flex flex-col flex-wrap gap-6 pb-10">
 				{events.map(({ attributes: { title, description, startDate } }, i) => (
 					<div
-						className={`rounded-lg p-5 ${colorSwitch(i)} text-sm drop-shadow-md`}
+						className={`rounded-lg p-5 ${colorSwitch(i)} text-sm drop-shadow-md `}
 						key={i}
 					>
 						<div className="flex items-center gap-3 mb-2">
