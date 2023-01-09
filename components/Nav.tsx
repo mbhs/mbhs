34418472328422527cef34755cb5df9669b12a76
Vijay Shanmugam <36575091+Jbolt01@users.noreply.github.com>
2023-01-09 11:28:53 -0500

    Make NavBar multilevel

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Nav() {
-	return (
-		<div className="h-16 flex justify-between items-center">
-			<div className="w-full fixed z-10 bg-red-600 p-3 flex text-white justify-between items-center">
-				<Link href="/">
-					<img alt="logo" src="/assets/logo.png" className="h-10 scale-110" />
-				</Link>
-				<div className="flex gap-3 items-center">
-					<Link href="/">Home</Link>
-					<Link href="/about">About</Link>
-					<Link href="/academies">Academies</Link>
-					<Link href="/news">News</Link>
-					<Link href="/schedule">Bell Schedule & Buses</Link>
-					<Link href="/schedule">Shortcuts</Link>
-				</div>
-			</div>
-		</div>
-	);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [navbarClass, setNavbarClass] = useState("");
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrollPosition(window.pageYOffset);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollPosition > 0) {
+      setNavbarClass("bg-red-600");
+    } else {
+      setNavbarClass("bg-red-600 p-3");
+    }
+  }, [scrollPosition]);
+
+  return (
+    <div className="h-16 flex justify-between items-center">
+      <div className={`w-full fixed z-10 ${navbarClass} flex text-white justify-between items-center`}>
+        <Link href="/">
+          <img alt="logo" src="/assets/logo.png" className="h-10 scale-110" />
+        </Link>
+        <div className="flex gap-3 items-center">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/academies">Academies</Link>
+          <Link href="/news">News</Link>
+          <Link href="/schedule">Bell Schedule & Buses</Link>
+          <Link href="/schedule">Shortcuts</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
