c091896607680979bfb069c648acef22b536e672
Asher Cronin <asher.lotke@gmail.com>
2023-03-06 11:43:16 -0500

    Add delay to animation and padding to the div

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,14 +4,36 @@ import { motion } from "framer-motion";
 
 export default function Nav() {
   const [scrollPosition, setScrollPosition] = useState(0);
-  const [navbarClass, setNavbarClass] = useState("bg-red-700");
-  const [mainDiv, setMainDiv] = useState("pb-16");
   const [scrollDir, setScrollDir] = useState(0);
+  const [navbarClass, setNavbarClass] = useState(["pb-16", "bg-red-700"]);
   const [isHover, setHover] = useState<{ [name: string]: boolean }>({});
 
-  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const hiddenMask = `repeating-linear-gradient(to top, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 999px, rgba(0,0,0,1) 999px, rgba(0,0,0,1) 999px)`;
+  const visibleMask = `repeating-linear-gradient(to top, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 999px)`;
 
   const subMenuAnimate = {
+    enter: {
+      opacity: 1,
+      maskImage: visibleMask,
+      WebkitMaskImage: visibleMask,
+      display: "flex"
+    },
+    exit: {
+      opacity: 0,
+      maskImage: hiddenMask,
+      WebkitMaskImage: hiddenMask,
+      transitionEnd: {
+        display: "none",
+      }
+    }
+  };
+
+  const subMenuTransition = {
+    duration: .35,
+    delay: .25
+  };
+
+  const subMenuAnimateNames = {
     enter: {
       originY: 0,
       opacity: 1,
@@ -37,6 +59,13 @@ export default function Nav() {
 
   const toggleHoverMenu = (which: string, state: boolean) => {
     let copy = { ...isHover };
+    if (which == "all") {
+      for (var key in copy) {
+        copy[key] = state;
+      }
+      setHover(isHover => ({ ...copy }));
+      return;
+    }
     for (var key in copy) {
       copy[key] = false;
     }
@@ -44,11 +73,13 @@ export default function Nav() {
     setHover(isHover => ({ ...copy }));
   };
 
-
   useEffect(() => {
     const threshold = 15;
     let lastScrollY = window.pageYOffset;
     const updateScrollDir = () => {
+
+      setScrollPosition(window.pageYOffset);
+
       const scrollY = window.pageYOffset;
       if (Math.abs(scrollY - lastScrollY) < threshold) {
         return;
@@ -56,7 +87,6 @@ export default function Nav() {
       setScrollDir(scrollY > lastScrollY ? -1 : 1);
       lastScrollY = scrollY > 0 ? scrollY : 0;
     };
-
     const onScroll = () => {
       window.requestAnimationFrame(updateScrollDir);
     };
@@ -65,39 +95,26 @@ export default function Nav() {
   }, [scrollDir]);
 
   useEffect(() => {
-    function handleScroll() {
-      setScrollPosition(window.pageYOffset);
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {
     if (scrollPosition < 80) {
-      setNavbarClass("bg-red-700");
-      setMainDiv("");
+      setNavbarClass(["hidden", "bg-red-700"]);
     } else {
-      setMainDiv("pb-20");
       if (scrollDir > 0) {
-        setNavbarClass("bg-red-700 fixed");
+        setNavbarClass(["", "bg-red-700 fixed"]);
       } else if (scrollDir < 0) {
-        setNavbarClass("hidden");
+        setNavbarClass(["", "hidden"]);
       }
-
     }
   }, [scrollPosition]);
 
   //TODO: make a hover-dropdown  component so that a pull from strapi can be mapped to links + dropdowns
   // also a component for a non dropdown (somehow switch between them depending on the need for a dropdown)
 
-
   return (
 
-    <div className={`${mainDiv} w-full flex flex-col`}>
-      <div className={`h-20 w-full ${navbarClass}`}>
-        <div className="px-8 flex flex-wrap items-center justify-between mx-auto">
+    <div className={`w-full flex flex-col`}>
+      <div className={`${navbarClass[0]} h-20 w-full bg-red-700`}></div>
+      <div className={`h-20 z-20 w-full ${navbarClass[1]}`}>
+        <div className="px-8 vi flex flex-wrap items-center justify-between mx-auto">
           <Link href="/" className="flex items-center">
             <img src="/assets/logo.png" className="h-6 m-3 sm:h-10" alt="Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white">MBHS</span>
@@ -106,8 +123,8 @@ export default function Nav() {
             <div className="my-1 flex flex-row items-center ml-auto px-auto">
               <motion.div onHoverEnd={() => toggleHoverMenu("students", false)}>
                 <Link onMouseEnter={() => toggleHoverMenu("students", true)} href="/students" className="block py-1 px-3 text-white">Students</Link>
-                <motion.div initial="exit" animate={isHover.students ? "enter" : "exit"} variants={subMenuAnimate}>
-                  <div className="mx-auto text-black font-semibold fixed bg-white">
+                <motion.div initial="exit" animate={isHover.students ? "enter" : "exit"} variants={subMenuAnimateNames}>
+                  <div className="mx-auto text-black font-semibold fixed bg-white rounded-lg">
                     <div>Vijay</div>
                     <div>Tinu</div>
                     <div>Asher</div>
@@ -116,82 +133,114 @@ export default function Nav() {
               </motion.div>
               <motion.div onHoverEnd={() => toggleHoverMenu("teachers", false)}>
                 <Link onMouseEnter={() => toggleHoverMenu("teachers", true)} href="/teachers" className="block py-1 px-3 text-white">Teachers</Link>
-                <motion.div initial="exit" animate={isHover.teachers ? "enter" : "exit"} variants={subMenuAnimate}>
-                  <div className="text-black font-semibold fixed bg-white">
+                <motion.div initial="exit" animate={isHover.teachers ? "enter" : "exit"} variants={subMenuAnimateNames}>
+                  <div className="mx-auto text-black font-semibold fixed bg-white rounded-lg">
                     <div>Mr. Foster</div>
                   </div>
                 </motion.div>
               </motion.div>
               <motion.div onHoverEnd={() => toggleHoverMenu("parents", false)}>
                 <Link onMouseEnter={() => toggleHoverMenu("parents", true)} href="/parents" className="block py-1 px-3 text-white">Parents</Link>
-                <motion.div initial="exit" animate={isHover.parents ? "enter" : "exit"} variants={subMenuAnimate}>
-                  <div className="text-black font-semibold fixed bg-white">
+                <motion.div initial="exit" animate={isHover.parents ? "enter" : "exit"} variants={subMenuAnimateNames}>
+                  <div className="mx-auto text-black font-semibold fixed bg-white rounded-lg">
                     <div>[parents]</div>
                   </div>
                 </motion.div>
               </motion.div>
             </div>
 
-            <div className="flex flex-row" onMouseEnter={() => setDropdownVisible(true)} onMouseLeave={() => setDropdownVisible(false)}>
-              <Link onMouseEnter={() => toggleHoverMenu("home", true)} onMouseLeave={() => toggleHoverMenu("home", false)} href="/" className="block py-1 px-3 text-white">Home</Link>
-              <Link onMouseEnter={() => toggleHoverMenu("about", true)} onMouseLeave={() => toggleHoverMenu("about", false)} href="/about" className="block py-1 px-3 text-white">About</Link>
-              <Link onMouseEnter={() => toggleHoverMenu("academies", true)} onMouseLeave={() => toggleHoverMenu("academies", false)} href="/academies" className="block py-1 px-3 text-white">Academies</Link>
-              <Link onMouseEnter={() => toggleHoverMenu("news", true)} onMouseLeave={() => toggleHoverMenu("news", false)} href="/news" className="block py-1 px-3 text-white">News</Link>
-              <Link onMouseEnter={() => toggleHoverMenu("schedule", true)} onMouseLeave={() => toggleHoverMenu("schedule", false)} href="/schedule" className="block py-1 px-3 text-white">Schedule</Link>
-            </div>
+            <div>
+              <div className="flex flex-row">
+                <Link onMouseOver={() => toggleHoverMenu("home", true)} onMouseLeave={() => toggleHoverMenu("home", false)} href="/" className="block py-1 px-3 text-white">Home</Link>
+                <Link onMouseOver={() => toggleHoverMenu("about", true)} onMouseLeave={() => toggleHoverMenu("about", false)} href="/about" className="block py-1 px-3 text-white">About</Link>
+                <Link onMouseOver={() => toggleHoverMenu("academies", true)} onMouseLeave={() => toggleHoverMenu("academies", false)} href="/academies" className="block py-1 px-3 text-white">Academies</Link>
+                <Link onMouseOver={() => toggleHoverMenu("news", true)} onMouseLeave={() => toggleHoverMenu("news", false)} href="/news" className="block py-1 px-3 text-white">News</Link>
+                <Link onMouseOver={() => toggleHoverMenu("schedule", true)} onMouseLeave={() => toggleHoverMenu("schedule", false)} href="/schedule" className="block py-1 px-3 text-white">Schedule</Link>
+              </div>
 
+              <motion.div
+                onMouseEnter={() => toggleHoverMenu("home", true)}
+                onMouseLeave={() => toggleHoverMenu("home", false)}
+                className="absolute mt-24 top-0 right-5 h-auto md:h-64 min-w-max w-auto md:w-[40%] flex flex-row bg-white rounded-lg"
+                initial={subMenuAnimate.exit}
+                animate={(isHover.home) ? subMenuAnimate.enter : subMenuAnimate.exit}
+                transition={subMenuTransition}>
+
+                <img src="/assets/MBHS_Entrance.jpg" className="rounded-l-lg" />
+                <div className="flex flex-col p-4">
+                  <div>Home</div>
+                </div>
+              </motion.div>
+
+              <motion.div
+                onMouseEnter={() => toggleHoverMenu("about", true)}
+                onMouseLeave={() => toggleHoverMenu("about", false)}
+                className="absolute mt-24 top-0 right-5 h-auto md:h-64 min-w-max w-auto md:w-[40%] flex flex-row bg-white rounded-lg"
+                initial={subMenuAnimate.exit}
+                animate={(isHover.about) ? subMenuAnimate.enter : subMenuAnimate.exit}
+                transition={subMenuTransition}>
+
+                <img src="/assets/drop-off-map.jpg" className="rounded-l-lg" />
+                <div className="flex flex-col p-4">
+                  <div>About</div>
+                </div>
+              </motion.div>
+
+              <motion.div
+                onMouseEnter={() => toggleHoverMenu("academies", true)}
+                onMouseLeave={() => toggleHoverMenu("academies", false)}
+                className="absolute mt-24 top-0 right-5 h-auto md:h-64 min-w-max w-auto md:w-[40%] flex flex-row bg-white rounded-lg"
+                initial={subMenuAnimate.exit}
+                animate={(isHover.academies) ? subMenuAnimate.enter : subMenuAnimate.exit}
+                transition={subMenuTransition}>
+
+                <img src="/assets/MBHS_Entrance.jpg" className="rounded-l-lg" />
+                <div className="flex flex-col p-4">
+                  <div>Magnet</div>
+                  <div>Cap</div>
+                  <div>STEM</div>
+                  <div>MMA</div>
+                  <div>Some other thing</div>
+                  <div>Some other thing2</div>
+                </div>
+              </motion.div>
+
+              <motion.div
+                onMouseEnter={() => toggleHoverMenu("news", true)}
+                onMouseLeave={() => toggleHoverMenu("news", false)}
+                className="absolute mt-24 top-0 right-5 h-auto md:h-64 min-w-max w-auto md:w-[40%] flex flex-row bg-white rounded-lg"
+                initial={subMenuAnimate.exit}
+                animate={(isHover.news) ? subMenuAnimate.enter : subMenuAnimate.exit}
+                transition={subMenuTransition}>
+
+                <img src="/assets/drop-off-map.jpg" className="rounded-l-lg" />
+                <div className="flex flex-col p-4">
+                  <div>TOP NEWS</div>
+                </div>
+              </motion.div>
+
+              <motion.div
+                onMouseEnter={() => toggleHoverMenu("schedule", true)}
+                onMouseLeave={() => toggleHoverMenu("schedule", false)}
+                className="absolute mt-24 top-0 right-5 h-auto md:h-64 min-w-max w-auto md:w-[40%] flex flex-row bg-white rounded-lg"
+                initial={subMenuAnimate.exit}
+                animate={(isHover.schedule) ? subMenuAnimate.enter : subMenuAnimate.exit}
+                transition={subMenuTransition}>
+
+                <img src="/assets/MBHS_Entrance.jpg" className="rounded-l-lg" />
+                <div className="flex flex-col p-4">
+                  <div>Regular Day</div>
+                  <div>Innovation Day</div>
+                  <div>Early Release Day</div>
+                  <div>2-Hour Delay Day</div>
+                  <div>All Period Day</div>
+                </div>
+              </motion.div>
+
+            </div>
           </div>
         </div>
       </div>
-      {dropdownVisible && <div className="fixed w-full z-50 mt-20 h-64">
-        <div className="w-full h-full z-50 flex flex-row-reverse">
-          <div className="w-[33%] z-50 rounded-lg mr-8">
-            <motion.div onMouseEnter={() => toggleHoverMenu("home", true)} onMouseLeave={() => toggleHoverMenu("home", false)} className="fixed h-64 w-full flex flex-row bg-white rounded-lg" initial="exit" animate={isHover.home ? "enter" : "exit"} variants={subMenuAnimate}>
-              <img src="/assets/MBHS_Entrance.jpg" className="rounded-l-lg" />
-              <div className="flex flex-col">
-                <div>Home</div>
-              </div>
-            </motion.div>
-
-            <motion.div onMouseEnter={() => toggleHoverMenu("about", true)} onMouseLeave={() => toggleHoverMenu("about", false)} className="fixed h-64 w-full flex flex-row bg-white rounded-lg" initial="exit" animate={isHover.about ? "enter" : "exit"} variants={subMenuAnimate}>
-              <img src="/assets/drop-off-map.jpg" className="rounded-l-lg" />
-              <div className="flex flex-col">
-                <div>About</div>
-              </div>
-            </motion.div>
-
-            <motion.div onMouseEnter={() => toggleHoverMenu("academies", true)} onMouseLeave={() => toggleHoverMenu("academies", false)} className="fixed h-64 w-full flex flex-row bg-white rounded-lg" initial="exit" animate={isHover.academies ? "enter" : "exit"} variants={subMenuAnimate}>
-              <img src="/assets/MBHS_Entrance.jpg" className="rounded-l-lg" />
-              <div className="flex flex-col">
-                <div>Magnet</div>
-                <div>Cap</div>
-                <div>STEM</div>
-                <div>MMA</div>
-                <div>Some other thing</div>
-                <div>Some other thing2</div>
-              </div>
-            </motion.div>
-
-            <motion.div onMouseEnter={() => toggleHoverMenu("news", true)} onMouseLeave={() => toggleHoverMenu("news", false)} className="fixed h-64 w-full flex flex-row bg-white rounded-lg" initial="exit" animate={isHover.news ? "enter" : "exit"} variants={subMenuAnimate}>
-              <img src="/assets/drop-off-map.jpg" className="rounded-l-lg" />
-              <div className="flex flex-col">
-                <div>TOP NEWS</div>
-              </div>
-            </motion.div>
-
-            <motion.div onMouseEnter={() => toggleHoverMenu("schedule", true)} onMouseLeave={() => toggleHoverMenu("schedule", false)} className="fixed h-64 w-full flex flex-row bg-white rounded-lg" initial="exit" animate={isHover.schedule ? "enter" : "exit"} variants={subMenuAnimate}>
-              <img src="/assets/MBHS_Entrance.jpg" className="rounded-l-lg" />
-              <div className="flex flex-col">
-                <div>Regular Day</div>
-                <div>Innovation Day</div>
-                <div>Early Release Day</div>
-                <div>2-Hour Delay Day</div>
-                <div>All Pediod Day</div>
-              </div>
-            </motion.div>
-          </div>
-        </div>
-      </div>}
     </div>
 
   );
