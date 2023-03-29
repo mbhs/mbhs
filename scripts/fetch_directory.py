87f1244dd8c944c135460fb9bef7796c9c88a773
Vijay Shanmugam <vijayrshan@gmail.com>
2023-03-29 10:58:37 -0400

    update fetch_directory.py to add teacher's to strapi

--- scripts/fetch_directory.py
+++ scripts/fetch_directory.py
@@ -1,23 +1,14 @@
 import json
-from bs4 import BeautifulSoup
 import requests
-from decouple import config
+from bs4 import BeautifulSoup
 
-# Load environment variables
-# dotenv.load_dotenv()
-
-def get_current_staff():
-    current_staff = requests.get('https://strapi.mbhs.edu/api/directory').json().get('data')
-    print(current_staff)
-    print(f'Found {len(current_staff)} current staff members')
-
-def parse_staff_directory_to_json(html_content, json_file_path):
+def parse_staff_directory_to_strapi(html_file_path, strapi_api_url):
+    # Read the HTML file
+    with open(html_file_path, 'r') as file:
+        html_content = file.read()
 
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Create a list to store the extracted staff information as dictionaries
-    staff_info_json = []
 
     # Find all department headers and iterate through them
     for department_header in soup.find_all('h5', class_='bg-cyan dark-gray-text py-2 pl-2'):
@@ -28,42 +19,35 @@ def parse_staff_directory_to_json(html_content, json_file_path):
             for li in ul.find_all('li'):
                 # Extract staff name, title, and email
                 name = li.find('span', class_='dark-gray-border').get_text(strip=True) if li.find('span', class_='dark-gray-border') else ''
-
                 staffinfo_element = li.find('div', class_='staffinfo clearfix mtm')
                 title = staffinfo_element.find('p').get_text(strip=True) if staffinfo_element else ''
-
                 email_element = li.find('a', href=lambda x: x and x.startswith('mailto:'))
                 email = email_element.get_text(strip=True) if email_element else ''
 
-                # Append the information to the staff_info_json list as a dictionary
-                staff_info_json.append({
-                  #'Department': department_name,
-                  'name': name,
-                  'title': title,
-                  'email': email,
-                  "createdBy": "Python Web Scraping Script",
-                  "updatedBy": "Python Web Scraping Script"
-                })
+                # Check if the staff member already exists in the Strapi database
+                response = requests.get(strapi_api_url, params={'email': email})
+                if response.status_code == 200 and response.json():
+                    print(f"Staff member {name} already exists in the database.")
+                    continue
 
+                # Prepare staff member data for POST request
+                staff_data = {
+                    'Department': department_name,
+                    'Name': name,
+                    'Title': title,
+                    'Email': email
+                }
 
-    print(staff_info_json)
-    for i in staff_info_json:
-      res = requests.post('https://strapi.mbhs.edu/api/directory', headers={
-        'Authorization': f'Bearer {config("STRAPI_API_KEY")}',
-      }, json={'data': i})
+                # POST request to add the staff member to the Strapi database
+                response = requests.post(strapi_api_url, json=staff_data)
+                if response.status_code == 201:
+                    print(f"Staff member {name} has been successfully added to the database.")
+                else:
+                    print(f"Failed to add staff member {name} to the database. Response code: {response.status_code}")
 
-    res.raise_for_status()
-    print(res)
+# Specify the path to the input HTML file and the Strapi API URL
+html_file_path = 'Staff Directory - Montgomery County Public Schools, Rockville, MD.html'
+strapi_api_url = 'https://strapi.mbhs.edu/api/directory'
 
-    # Save the extracted data to a JSON file
-    # with open(json_file_path, 'w') as jsonfile:
-    #     json.dump(staff_info_json, jsonfile, indent=4)
-
-# Specify the path to the input HTML file and the output JSON file
-html = requests.get('https://ww2.montgomeryschoolsmd.org/directory/directory_Boxschool.aspx?processlevel=04757').text
-json_file_path = 'teachers.json'
-
-# Execute the function to parse the HTML and save the data to JSON
-parse_staff_directory_to_json(html, json_file_path)
-#get_current_staff()
-print(config("STRAPI_API_KEY"))
+# Execute the function to parse the HTML and add the staff data to the Strapi database
+parse_staff_directory_to_strapi(html_file_path, strapi_api_url)
