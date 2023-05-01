26367c08ab6347e6013fa160e2e6ea90ffaa2c67
Tinu V <tinuvanapamula@gmail.com>
2023-05-01 23:55:08 +0000

    update build script for bonx

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,13 @@
 git pull
 
+old=$(docker ps -a -q  --filter ancestor=mbhs)
+
+#docker rm $(docker stop $(docker ps -a -q  --filter ancestor=mbhs))
+
 docker build . -t mbhs
 
-docker run . -p 3000:3000 -t mbhs
+docker stop $old
+
+docker run -d -p 3000:3000 mbhs
+
+docker rm $old
