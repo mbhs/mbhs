8016e0b2cab03d9366a41e5c23a06822ae519665
Asher Cronin <46426339+albgje@users.noreply.github.com>
2023-09-21 13:35:59 -0400

    manually focus quicklink button

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -209,6 +209,8 @@ export default function Nav({
 	const [mobileNav, setMobileNav] = useState(false);
 	const [data, setData] = useState<LinkType[]>();
 
+	const quickLinkButton = useRef<HTMLButtonElement>(null);
+
 	const fetchLinks = async () => {
 		// fetch data from strapi
 		await fetch("https://strapi.mbhs.edu/api/links?filters&sort=order:ASC&sort=name:ASC")
@@ -392,7 +394,8 @@ export default function Nav({
 								</motion.p>
 							))}
 						<motion.button
-							onClick={() => setDropdownOpen((prev) => !prev)}
+							onClick={() => {setDropdownOpen((prev) => !prev), quickLinkButton.current?.focus()}}
+							ref={quickLinkButton}
 							onBlur={() => setDropdownOpen(false)}
 							initial={{ opacity: 0 }}
 							animate={{ opacity: 1 }}
