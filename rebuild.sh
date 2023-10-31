140addce11eea9034305762408b7f05f1db7a8c1
Asher Cronin <46426339+albgje@users.noreply.github.com>
2023-10-31 14:27:20 -0400

    delete old docker images

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,17 @@
 git pull
 
-old=$(docker ps -a -q  --filter ancestor=mbhs)
+old_container=$(docker ps -a -q  --filter ancestor=mbhs)
+
+old_image=$(docker images -q mbhs)
 
 #docker rm $(docker stop $(docker ps -a -q  --filter ancestor=mbhs))
 
 docker build . -t mbhs --no-cache
 
-docker stop $old
+docker stop $old_container
 
 docker run -d -p 3000:3000 mbhs
 
-docker rm $old
+docker rm $old_container
+
+docker rmi $old_image
