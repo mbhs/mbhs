f84d8f7de8d3756f008ef16c2f45fe16f2df8067
Asher Cronin <46426339+albgje@users.noreply.github.com>
2023-11-07 08:16:16 -0500

    correct order by startTime on home page too

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,12 +37,12 @@ export async function getStaticProps() {
 
 	console.log(todayStr, nextWeek);
 	let events = await fetch(
-		`https://strapi.mbhs.edu/api/events?filters[$or][0][endDate][$gte]=${todayStr}&filters[$or][1][$and][0][endDate][$null]=true&filters[$or][1][$and][1][startDate][$gte]=${todayStr}&filters[$and][3][startDate][$lte]=${nextWeek}&sort=startDate:ASC&filters[important]=true`
+		`https://strapi.mbhs.edu/api/events?filters[$or][0][endDate][$gte]=${todayStr}&filters[$or][1][$and][0][endDate][$null]=true&filters[$or][1][$and][1][startDate][$gte]=${todayStr}&filters[$and][3][startDate][$lte]=${nextWeek}&sort=startDate:ASC&sort=startTime:ASC&filters[important]=true`
 	).then((res) => res.json());
 
 	if (events!.meta.pagination.total < 3) {
 		events = await fetch(
-			`https://strapi.mbhs.edu/api/events?filters[startDate][$gte]=${todayStr}&sort=startDate:ASC&filters[important]=true&pagination[pageSize]=3`
+			`https://strapi.mbhs.edu/api/events?filters[startDate][$gte]=${todayStr}&sort=startDate:ASC&sort=startTime:ASC&filters[important]=true&pagination[pageSize]=3`
 		).then((res) => res.json());
 	}
 
