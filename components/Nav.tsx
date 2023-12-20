4f2e30d17297fcd76672e72f3fe8e6a6e1f0e282
Asher Cronin <46426339+albgje@users.noreply.github.com>
2023-12-19 19:32:33 -0500

    fix home button

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -372,7 +372,7 @@ export default function Nav({
 				className={`h-16 sm:h-20 z-20 w-full ${navbarClass[1]} px-3 md:px-8 flex flex-wrap items-center justify-between mx-auto`}
 			>
 				<Link
-					href={`/${lang}`}
+					href={`/${lang}/home`}
 					className="flex -mt-1 z-10 items-center"
 					onClick={() => setMobileNav(false)}
 				>
